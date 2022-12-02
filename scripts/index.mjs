a72823f38953d014a8fe05490426831fce09108a
Shagun20-03 <goyalshagun03@gmail.com>
2022-12-02 23:23:13 +0530

    added home and sign up page

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -1,14 +1,13 @@
 
 import { createNewProduct } from "./utils.js"
 // Alert the user when they "bought" a product
-$("#card-container").on("click", "#buy-btn", function () {
-    alert("Thank you for adding product");
-});
 
 // Handle page changes to add-product.html
 $("#add-product").on("click", function () {
     window.location.href = "add-product.html";
 });
+
+$("#")
 
 // Handle form submit for add-product.html
 $("#product-form").submit(async function (form) {
@@ -31,11 +30,4 @@ $("#product-form").submit(async function (form) {
 
 $("#get-product").on("click", function () {
     window.location.href = "get-product.html";
-});
-
-$("#card-container").on("click", "#edit-btn", function () {
-    // Get the id of the product
-    var productID = $(this).parent().parent().parent().attr("id");
-    var url = "update-product.html" + "?id=" + productID;
-    window.location.href = url;
 });
